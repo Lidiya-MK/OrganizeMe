911dc8e5d7d55eaea21428344d60e18bb3ee0e40
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-10-05 15:52:18 +0300

    added star task   method

--- controller/userController.js
+++ controller/userController.js
@@ -133,3 +133,18 @@ const createToken = (id) => {
       res.status(400).json({ error: 'Task deletion failed' });
     }
   };
+
+
+  const starTask = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const task = await Task.findOne({ _id: id, owner: req.user._id });
+      if (!task) return res.status(404).json({ error: 'Task not found' });
+      task.isImportant = !task.isImportant;
+      await task.save();
+      res.status(200).json(task);
+    } catch (error) {
+      res.status(400).json({ error: 'Failed to star the task' });
+    }
+  };
+  
