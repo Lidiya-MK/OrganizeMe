0d18a3b987bea5229e3d8afeaafdf543b6506974
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-10-05 15:36:10 +0300

    added createToken for user controllers

--- controller/userController.js
+++ controller/userController.js
@@ -2,3 +2,9 @@ const jwt = require('jsonwebtoken');
 const User = require('../model/User');
 const Task = require('../model/Task');
 const fs = require('fs');
+
+
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+  };
+  
