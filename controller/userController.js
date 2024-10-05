d2b9625047af53d27b5c10692d4d54be74e9ab32
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-10-05 15:56:11 +0300

    added getProPic   method

--- controller/userController.js
+++ controller/userController.js
@@ -212,3 +212,22 @@ const createToken = (id) => {
       res.status(400).json({ error: 'Failed to update user' });
     }
   };
+
+
+  const path = require('path');
+
+const getUserProfilePicture = async (req, res) => {
+  try {
+      const userId = req.params.id; 
+
+
+      const user = await User.findById(userId);
+
+      if (!user || !user.profilePicture) {
+          return res.status(404).json({ message: "Profile picture not found" });
+      }
+      res.status(200).json({ profilePicture: user.profilePicture });
+  } catch (err) {
+      res.status(500).json({ message: "Error fetching profile picture", error: err.message });
+  }
+};
