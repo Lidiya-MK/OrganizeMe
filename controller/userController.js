1b14efc56fc46ba3ea5c5c07a3c9d227df82ad23
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-10-05 15:51:01 +0300

    added update Task method

--- controller/userController.js
+++ controller/userController.js
@@ -108,3 +108,16 @@ const createToken = (id) => {
       res.status(400).json({ error: 'Failed to fetch tasks' });
     }
   };
+
+
+  const updateTask = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const updates = req.body;
+      const task = await Task.findOneAndUpdate({ _id: id, owner: req.user._id }, updates, { new: true });
+      if (!task) return res.status(404).json({ error: 'Task not found' });
+      res.status(200).json(task);
+    } catch (error) {
+      res.status(400).json({ error: 'Task update failed' });
+    }
+  };
