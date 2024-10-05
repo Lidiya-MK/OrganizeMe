b3a7b95ca042d579e06e78b8e352ea73e08edac3
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-10-05 15:56:55 +0300

    added updateProPic   method

--- controller/userController.js
+++ controller/userController.js
@@ -231,3 +231,32 @@ const getUserProfilePicture = async (req, res) => {
       res.status(500).json({ message: "Error fetching profile picture", error: err.message });
   }
 };
+
+
+
+const updateProfilePicture = async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const user = await User.findById(userId);
+  
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+  
+
+        if (req.file) {
+            const profilePicturePath = `/uploads/${req.file.filename}`;
+            
+
+            user.profilePicture = profilePicturePath;
+            await user.save();
+  
+            return res.status(200).json({ message: 'Profile picture updated', profilePicture: profilePicturePath });
+        } else {
+            return res.status(400).json({ message: 'No file uploaded' });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: 'Error updating profile picture', error: error.message });
+    }
+  };
+  
