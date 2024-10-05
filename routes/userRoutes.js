333a68ec734e43403b93e79dd8c1aacd1f406eb2
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-10-05 21:23:37 +0300

    finished setting up routes for users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const { authenticateUser } = require('../middleware/auth');
-
-const path = require('path');
+const multer = require('multer');
 const {
   signupUser,
   loginUser,
-
+  createTask,
+  updateTask,
+  deleteTask,
+  starTask,
+  getUser,
+  markTaskDone,
+  getImportantTasks,
+  getUserTasks,
+  updateUser,
+  getUserProfilePicture,
+  updateProfilePicture
  
 } = require('../controller/userController');
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); 
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
 
 
 
@@ -15,6 +38,17 @@ router.post('/signup', signupUser);
 router.post('/login', loginUser);  
 
 
+router.post('/tasks', authenticateUser, createTask);          
+router.put('/tasks/:id', authenticateUser, updateTask);      
+router.delete('/tasks/:id', authenticateUser, deleteTask);   
+router.put('/tasks/:id/star', authenticateUser, starTask);    
+router.put('/tasks/:id/done', authenticateUser, markTaskDone); 
+router.get('/:id', authenticateUser, getUser);
+router.get('/tasks/:id', authenticateUser, getUserTasks);
+router.get('/tasks/important', authenticateUser, getImportantTasks);
+router.patch('/update',  authenticateUser,  updateUser);
+router.get('/profile-picture/:id', getUserProfilePicture);
+router.patch('/profile-picture', authenticateUser, upload.single('profilePicture'), updateProfilePicture);
 
 
 
