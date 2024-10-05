69c7fc0bc60cb93e45d0e8b1571df7fdad325f89
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-10-05 15:38:33 +0300

    added login for user

--- controller/userController.js
+++ controller/userController.js
@@ -37,3 +37,21 @@ const createToken = (id) => {
     }
   };
   
+  const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+        const user = await User.findOne({ email });
+  
+        if (!user || !(await user.comparePassword(password))) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+  
+       
+    
+  
+        res.json({ token, userId: user._id }); 
+    } catch (error) {
+        res.status(500).json({ message: 'Login failed. Please try again.' });
+    }
+  };
