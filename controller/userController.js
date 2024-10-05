541fa3635c313876f651355ee20061bf676ac993
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-10-05 15:55:00 +0300

    added updateUser   method

--- controller/userController.js
+++ controller/userController.js
@@ -186,3 +186,29 @@ const createToken = (id) => {
       res.status(400).json({ error: 'Failed to fetch important tasks' });
     }
   };
+
+
+
+
+  const updateUser = async (req, res) => {
+    try {
+      const userId = req.user._id;
+  
+      const { userName, email } = req.body; 
+
+      const updatedUser = await User.findByIdAndUpdate(
+        userId,
+        { userName, email },
+        { new: true, runValidators: true } 
+      );
+  
+      if (!updatedUser) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+  
+      res.status(200).json({ message: 'User updated successfully', user: updatedUser });
+    } catch (error) {
+      console.error('Error updating user:', error);
+      res.status(400).json({ error: 'Failed to update user' });
+    }
+  };
