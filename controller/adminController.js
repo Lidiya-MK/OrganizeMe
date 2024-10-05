c45a5c181b849ec43a7f21e5a447336a09b0cbef
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-10-05 15:33:59 +0300

    added update user  controller for admin

--- controller/adminController.js
+++ controller/adminController.js
@@ -36,6 +36,18 @@ const loginAdmin = async (req, res) => {
   };
   
 
+  const updateUser = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const updates = req.body;
+      const user = await User.findByIdAndUpdate(id, updates, { new: true });
+      if (!user) return res.status(404).json({ error: 'User not found' });
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(400).json({ error: 'User update failed' });
+    }
+  };
+
 
 
 module.exports = {
