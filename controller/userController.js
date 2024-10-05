2576d79395a8e80759a9e5392879023440f39390
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-10-05 15:51:46 +0300

    added delete task  method

--- controller/userController.js
+++ controller/userController.js
@@ -121,3 +121,15 @@ const createToken = (id) => {
       res.status(400).json({ error: 'Task update failed' });
     }
   };
+
+
+  const deleteTask = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const task = await Task.findOneAndDelete({ _id: id, owner: req.user._id });
+      if (!task) return res.status(404).json({ error: 'Task not found' });
+      res.status(200).json({ message: 'Task deleted successfully' });
+    } catch (error) {
+      res.status(400).json({ error: 'Task deletion failed' });
+    }
+  };
