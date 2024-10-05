c659d220d1f86af7bafda7095f89d5de64836cb5
Lidiya Mamo Kibret <133108747+Lidiya-MK@users.noreply.github.com>
2024-10-05 15:37:14 +0300

    added sign up contoller for user

--- controller/userController.js
+++ controller/userController.js
@@ -8,3 +8,19 @@ const createToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1d' });
   };
   
+
+
+  const signupUser = async (req, res) => {
+    try {
+      const { userName,email, password} = req.body;
+      const user = new User({ userName,email, password });
+      await user.save();
+      const token = createToken(user._id);
+      res.status(201).json({ token });
+    } catch (error) {
+      res.status(400).json({ error: 'User signup failed' });
+    }
+  };
+
+
+  
